52a270a21c3d86d5263c4c45dee5b8d4b8e8f6a5
KetlynLaury <ketlynlaury2017@gmail.com>
2024-07-26 11:13:05 -0300

    avatar responsivo e view

--- src/assets/components/pages/Home.jsx
+++ src/assets/components/pages/Home.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import '../styles/home.css'; 
 
 const Home = () => {
   return (
     <>
+        <div className="home-container">
       <div>Home</div>
       <p>testando</p>
+      </div>     
     </>
   )
 }
--- src/assets/components/pages/settings.jsx
+++ src/assets/components/pages/settings.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import '../styles/settings.css'; 
 
 const Settings = () => {
   return (
+    <div className="settings-container">
     <div>Settings</div>
+    </div>
   )
 }
 
